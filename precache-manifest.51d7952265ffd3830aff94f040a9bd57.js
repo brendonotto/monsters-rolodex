24cbda212f1990033c496fc295e18aefca198b72
Brendon Otto <brendon@brendonotto.com>
2019-10-20 21:48:48 -0500

    Updates

---
+++ precache-manifest.51d7952265ffd3830aff94f040a9bd57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "858e3e0305755016fb85074354f899c3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "56808257329b1059eaf7",
+    "url": "/monsters-rolodex/static/css/main.ca9f152f.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monsters-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "56808257329b1059eaf7",
+    "url": "/monsters-rolodex/static/js/main.612a0a48.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
